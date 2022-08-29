d40ee50e09d25cbe39d292ce86cd320ce4ad6342
YuriAzathoth <iamkiller@mail.com>
2022-08-29 11:26:56 +0300

    Scene hierarchy components and system basic implementation

---
+++ Engine/Scene/SceneComponents.cpp
@@ -0,0 +1,35 @@
+/*
+	Apokalypse3D - Fast and cache-friendly 3D game engine
+	Copyright (C) 2022 Yuriy Zinchenko
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "SceneComponents.h"
+
+using namespace Components::Scene;
+
+SceneComponents::SceneComponents(flecs::world& world)
+{
+	flecs::_::cpp_type<SceneComponents>::id_explicit(world);
+	world.module<SceneComponents>("Components::Scene");
+
+	translation_ = world.component<Translation>();
+
+	look_ = world.component<Look>();
+	move_ = world.component<Move>();
+	node_ = world.component<Node>();
+	root_ = world.component<Root>().add(flecs::Tag);
+	rotate_ = world.component<Rotate>();
+}
