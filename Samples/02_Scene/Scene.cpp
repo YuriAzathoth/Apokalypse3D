7ebe047c0eb0b93584831a550b512456cf61ccec
YuriAzathoth <iamkiller@mail.com>
2022-08-31 15:57:29 +0300

    Basic SDL Events processor module

--- Samples/02_Scene/Scene.cpp
+++ Samples/02_Scene/Scene.cpp
@@ -16,179 +16,106 @@
 	along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-#include <flecs.h>
+
+#include <bgfx/bgfx.h>
 #include <glm/gtx/quaternion.hpp>
-#include <glm/trigonometric.hpp>
-#include "Core/Coordinator.h"
-#include "Core/MainThreadSystem.h"
-#include "Graphics/Graphics.h"
-#include "Graphics/GraphicsComponents.h"
-#include "Scene/Scene.h"
+#include <glm/glm.hpp>
+#include <thread>
+#include "Core/Engine.h"
+#include "Debug/DebugModelRendererSystems.h"
+#include "Graphics/CameraComponents.h"
+#include "Graphics/CameraSystems.h"
+#include "Graphics/GraphicsResourceComponents.h"
+#include "Graphics/GraphicsResourceSystems.h"
+#include "Graphics/RendererComponents.h"
+#include "Graphics/RendererSystems.h"
+#include "Graphics/WindowComponents.h"
+#include "Graphics/WindowSystems.h"
+#include "Input/EventComponents.h"
+#include "Input/EventSystems.h"
 #include "Scene/SceneComponents.h"
+#include "Scene/SceneSystems.h"
 
-struct Vertex
-{
-	float x;
-	float y;
-	float z;
-	float u;
-	float v;
-};
+using namespace A3D;
+using namespace A3D::Components;
+using namespace ::Components;
 
-static constexpr Vertex VERTICES[] = {
-	{-1.0f, 1.0f, 1.0f, 0.0f, 0.0f},	// LUF
-	{1.0f, 1.0f, 1.0f, 1.0f, 0.0f},		// RUF
-	{-1.0f, -1.0f, 1.0f, 0.0f, 1.0f},	// LDF
-	{1.0f, -1.0f, 1.0f, 1.0f, 1.0f},	// RDF
-	{-1.0f, 1.0f, -1.0f, 1.0f, 0.0f},	// LUB
-	{1.0f, 1.0f, -1.0f, 0.0f, 0.0f},	// RUB
-	{-1.0f, -1.0f, -1.0f, 1.0f, 1.0f},	// LDB
-	{1.0f, -1.0f, -1.0f, 0.0f, 1.0f},	// RDB
-	{-1.0f, 1.0f, -1.0f, 0.0f, 1.0f},	// LUB
-	{1.0f, 1.0f, -1.0f, 1.0f, 1.0f},	// RUB
-	{-1.0f, -1.0f, -1.0f, 0.0f, 0.0f},	// LDB
-	{1.0f, -1.0f, -1.0f, 1.0f, 0.0f},	// RDB
-};
-
-static constexpr uint16_t INDICES[] = {
-	0, 1, 2, 1, 3, 2, // Front
-	4, 6, 5, 5, 6, 7, // Back
-	0, 2, 4, 4, 2, 6, // Left
-	1, 5, 3, 5, 7, 3, // Right
-	0, 8, 1, 8, 9, 1, // Top
-	2, 3, 10, 10, 3, 11, // Bottom
-};
-
-void CreateBoxes(flecs::world& world,
-				 flecs::entity parent,
-				 const Model& model,
-				 const MaterialBasic& material,
+void CreateBoxes(flecs::entity parent,
+				 const char* model,
+				 const char* vertex,
+				 const char* fragment,
 				 float scale,
-				 unsigned levels)
+				 unsigned children,
+				 unsigned levels,
+				 glm::mat4 transform = glm::mat4{1.0f})
 {
-	glm::mat4 identity(1.0f);
-	const glm::vec3 dist(5.0f, 0.0f, 0.0f);
-	const glm::vec3 size(scale, scale, scale);
-	glm::mat4 tr = glm::scale(glm::translate(identity, dist), size);
-	flecs::entity box = world.entity("Model1")
-							  .child_of(parent)
-							  .add<Node>()
-							  .set<Translation>({tr})
-							  .set<Model>(model)
-							  .set<MaterialBasic>(material)
-							  .set<Rotate>({{glm::radians(90.0f), glm::radians(90.0f), 0.0f}});
+	flecs::entity cube = parent.world().entity().child_of(parent)
+		.add<Scene::Node>()
+		.set<Scene::Translation>({transform})
+		.set<Scene::Rotate>({{0.0f, 0.0f, 1.0f}})
+		.set<Resource::Graphics::GetModelFile>({model})
+		.set<Resource::Graphics::GetGpuProgram>({vertex, fragment});
 
-	glm::quat rot = glm::angleAxis(glm::radians(90.0f), glm::vec3{0.0f, 0.0f, 1.0f});
-	tr = glm::scale(glm::translate(identity, rot * dist), size);
-	box = world.entity("Model2")
-				.child_of(parent)
-				.add<Node>()
-				.set<Translation>({tr})
-				.set<Model>(model)
-				.set<MaterialBasic>(material)
-				.set<Rotate>({{glm::radians(90.0f), 0.0f, glm::radians(90.0f)}});
-
-	rot = glm::angleAxis(glm::radians(180.0f), glm::vec3{0.0f, 0.0f, 1.0f});
-	tr = glm::scale(glm::translate(identity, rot * dist), size);
-	box = world.entity("Model3")
-				.child_of(parent)
-				.add<Node>()
-				.set<Translation>({tr})
-				.set<Model>(model)
-				.set<MaterialBasic>(material)
-				.set<Rotate>({{glm::radians(-90.0f), glm::radians(-90.0f), 0.0f}});
-
-	rot = glm::angleAxis(glm::radians(270.0f), glm::vec3{0.0f, 0.0f, 1.0f});
-	tr = glm::scale(glm::translate(identity, rot * dist), size);
-	box = world.entity("Model4")
-				.child_of(parent)
-				.add<Node>()
-				.set<Translation>({tr})
-				.set<Model>(model)
-				.set<MaterialBasic>(material)
-				.set<Rotate>({{glm::radians(-90.0f), 0.0f, glm::radians(-90.0f)}});
-
-	if (levels)
-		parent.children([&world, &model, material, levels](flecs::entity child)
+	if (levels > 1)
+	{
+		const glm::vec3 pos{5.0f, 0.0f, 0.0f};
+		const glm::vec3 size(scale, scale, scale);
+		const float angle_step = glm::radians(360.0f / (float)children);
+		glm::quat rot;
+		scale *= 0.5f;
+		for (float i = 0.0f; i < (float)children; ++i)
 		{
-			CreateBoxes(world, child, model, material, 0.75f, levels - 1);
-		});
+			rot = glm::angleAxis(angle_step * i, glm::vec3{0.0f, 0.0f, 1.0f});
+			transform = glm::scale(glm::translate(glm::toMat4(rot), pos), size);
+			CreateBoxes(cube, model, vertex, fragment, scale, children, levels - 1, transform);
+		}
+	}
 }
 
 int main()
 {
-	flecs::world world;
-	world.set_threads(4);
-	RegisterGraphics(world);
-	RegisterMainThreadSystem(world);
-	RegisterScene(world);
+	Engine engine;
+	flecs::world& world = engine.get_world();
+	world.import<CameraComponents>();
+	world.import<EventComponents>();
+	world.import<GraphicsResourceComponents>();
+	world.import<SceneComponents>();
+	world.import<RendererComponents>();
+	world.import<WindowComponents>();
+	world.import<CameraSystems>();
+	world.import<EventSystems>();
+	world.import<GraphicsResourceSystems>();
+	world.import<RendererSystems>();
+	world.import<WindowSystems>();
+	world.import<SceneSystems>();
+	world.import<DebugModelRendererSystems>();
 
-	world.set<WindowCreate>({"02_Scene"});
-	Run(world);
+	Renderer::RendererConfig renderer{};
+	renderer.multi_threaded = true;
+	world.set<Renderer::RendererConfig>(renderer);
+	world.add<Window::WindowConfig>();
+	create_video(world);
+	create_window(world);
+	create_renderer(world);
 
-	/*Coordinator::InitInfo initInfo;
-	initInfo.graphics.title = "Hello World";
-	initInfo.graphics.render = Graphics::RenderType::OpenGL;
-	initInfo.graphics.msaa = Graphics::MSAA::NONE;
-	initInfo.graphics.display = 0;
-	initInfo.graphics.width = 1024;
-	initInfo.graphics.height = 768;
-	initInfo.graphics.fullscreen = false;
-	initInfo.graphics.highDpi = true;
-	initInfo.graphics.vsync = true;
+	flecs::entity scene = world.entity()
+						  .add<Scene::Root>()
+						  .add<Scene::Node>()
+						  .add<Scene::Translation>();
 
-	bool initialized;
-	Coordinator coordinator(initInfo, initialized);
-	if (!initialized)
-		return 1;
+	flecs::entity camera = world.entity().child_of(scene)
+						   .add<Scene::Node>()
+						   .set<Scene::Translation>({glm::translate(glm::mat4(1.0f), {0.0f, 0.0f, -50.0f})})
+						   .add<Camera::Eye>()
+						   .set<Camera::Perspective>({glm::radians(45.0f), 0.01f, 100.0f})
+						   .add<Camera::Viewport>();
 
-	Graphics& graphics = coordinator.GetGraphics();
-	bgfx::VertexBufferHandle vbo = graphics.CreateVertexBuffer(VERTICES, sizeof(VERTICES));
-	bgfx::IndexBufferHandle ebo = graphics.CreateIndexBuffer(INDICES, sizeof(INDICES));
-	bgfx::ShaderHandle vertex = graphics.LoadShader("01_hello_world.vert.shader");
-	bgfx::ShaderHandle fragment = graphics.LoadShader("01_hello_world.frag.shader");
-	bgfx::ProgramHandle program = graphics.CreateProgram(vertex, fragment);
-	bgfx::TextureHandle texture = graphics.LoadTexture("SampleCrate.ktx");
-	const Model model = {vbo, ebo};
-	const MaterialBasic material = { program, texture };
+	constexpr const char MODEL[] = "../Data/Models/Box.mdl";
+	constexpr const char VERTEX_SHADER[] = "white_solid.vert";
+	constexpr const char FRAGMENT_SHADER[] = "white_solid.frag";
+	CreateBoxes(scene, MODEL, VERTEX_SHADER, FRAGMENT_SHADER, 1.0f, 8, 4);
 
-	if (!(bgfx::isValid(vbo) &&
-		  bgfx::isValid(ebo) &&
-		  bgfx::isValid(vertex) &&
-		  bgfx::isValid(fragment) &&
-		  bgfx::isValid(program) &&
-		  bgfx::isValid(texture)))
-		return 1;
-
-	flecs::world& world = coordinator.GetWorld();
-	flecs::entity scene = coordinator.GetScene().GetRoot();
-
-	flecs::entity camera = world.entity("Camera")
-							   .child_of(scene)
-							   .add<Node>()
-							   .add<Camera>()
-							   .set<Perspective>({glm::radians(45.0f), 0.0f, 0.1f, 100.0f})
-							   .set<Ray>({{0.0f, 0.0f, 20.0f}, {0.0f, 0.0f, 0.0f}})
-							   .add<WindowAspect>();
-
-	flecs::entity origin = world.entity("Origin")
-								.child_of(scene)
-								.add<Node>()
-								.add<Translation>()
-								.set<Model>(model)
-								.set<MaterialBasic>(material)
-								.set<Rotate>({{0.0f, 0.0f, glm::radians(45.0f)}});
-
-	CreateBoxes(world, origin, model, material, 0.75f, 4);*/
-
-	//coordinator.Run();
-
-	/*bgfx::destroy(vertex);
-	bgfx::destroy(fragment);
-	bgfx::destroy(program);
-	bgfx::destroy(vbo);
-	bgfx::destroy(ebo);
-	bgfx::destroy(texture);*/
+	engine.Run();
 
 	return 0;
 }
