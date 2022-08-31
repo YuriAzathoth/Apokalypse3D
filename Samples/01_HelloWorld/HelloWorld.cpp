a5dd6fbc735ee30c8fa790ceff0e5c9e3d79f00b
YuriAzathoth <iamkiller@mail.com>
2022-08-31 15:59:24 +0300

    Added Hello World sample (just shows black window)

---
+++ Samples/01_HelloWorld/HelloWorld.cpp
@@ -0,0 +1,52 @@
+/*
+	Apokalypse3D - Fast and cache-friendly 3D game engine
+	Copyright (C) 2022 Yuriy Zinchenko
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "Core/Engine.h"
+#include "Graphics/RendererComponents.h"
+#include "Graphics/RendererSystems.h"
+#include "Graphics/WindowComponents.h"
+#include "Graphics/WindowSystems.h"
+#include "Input/EventComponents.h"
+#include "Input/EventSystems.h"
+
+using namespace A3D;
+using namespace A3D::Components;
+
+int main()
+{
+	Engine engine;
+	flecs::world& world = engine.get_world();
+	world.import<EventComponents>();
+	world.import<RendererComponents>();
+	world.import<WindowComponents>();
+	world.import<EventSystems>();
+	world.import<RendererSystems>();
+	world.import<WindowSystems>();
+
+	Renderer::RendererConfig renderer;
+	renderer.multi_threaded = true;
+	world.set<Renderer::RendererConfig>(renderer);
+	world.add<Window::WindowConfig>();
+
+//	world.add<Renderer::RendererConfig>();
+//	world.add<Window::WindowConfig>();
+	create_video(world);
+	create_window(world);
+	create_renderer(world);
+	return engine.Run();
+}
