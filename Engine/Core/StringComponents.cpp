4cea0c3f7d8b4c62311483316178bb89f6425e4d
YuriAzathoth <iamkiller@mail.com>
2022-08-31 15:56:29 +0300

    Basic string keeper implementation

---
+++ Engine/Core/StringComponents.cpp
@@ -0,0 +1,31 @@
+/*
+	Apokalypse3D - Fast and cache-friendly 3D game engine
+	Copyright (C) 2022 Yuriy Zinchenko
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "StringComponents.h"
+
+using namespace A3D::Components::Str;
+
+namespace A3D
+{
+StringComponents::StringComponents(flecs::world& world)
+{
+	flecs::_::cpp_type<StringComponents>::id_explicit(world, 0, false);
+	world.module<StringComponents>("A3D::Components::Str");
+	stringView_ = world.component<StringView>().member<const char*>("str");
+}
+} // namespace A3D
