6e4d6da673e55193b13db2f17cd691983c1c82c7
YuriAzathoth <iamkiller@mail.com>
2022-11-13 16:49:01 +0200

    Handle file and line in log functions

--- Engine/IO/Log.h
+++ Engine/IO/Log.h
@@ -23,24 +23,32 @@
 
 //#define LOG_LEVEL_TRACE_ENABLED
 
+#ifdef NDEBUG
 #define LogError(FORMAT, ...) LogWrite(LOG_LEVEL_ERROR, FORMAT, ##__VA_ARGS__)
 #define LogWarning(FORMAT, ...) LogWrite(LOG_LEVEL_WARNING, FORMAT, ##__VA_ARGS__)
 #define LogInfo(FORMAT, ...) LogWrite(LOG_LEVEL_INFO, FORMAT, ##__VA_ARGS__)
-
-#ifndef NDEBUG
-#define LogDebug(FORMAT, ...) LogWrite(LOG_LEVEL_DEBUG, FORMAT, ##__VA_ARGS__)
-#define DebugFor(EXPR, BODY) for EXPR BODY
-#define DebugIf(EXPR, BODY) if (EXPR) BODY
-#else // NDEBUG
 #define LogDebug(FORMAT, ...)
+#define Assert(...)
 #define DebugFor(EXPR, BODY)
 #define DebugIf(EXPR, BODY)
+#else // NDEBUG
+#define LogError(FORMAT, ...) LogWrite(LOG_LEVEL_ERROR, __FILE__, __LINE__, FORMAT, ##__VA_ARGS__)
+#define LogWarning(FORMAT, ...) LogWrite(LOG_LEVEL_WARNING, __FILE__, __LINE__, FORMAT, ##__VA_ARGS__)
+#define LogInfo(FORMAT, ...) LogWrite(LOG_LEVEL_INFO, __FILE__, __LINE__, FORMAT, ##__VA_ARGS__)
+#define LogDebug(FORMAT, ...) LogWrite(LOG_LEVEL_DEBUG, __FILE__, __LINE__, FORMAT, ##__VA_ARGS__)
+#define Assert(CONT, FORMAT, ...) \
+if (!(CONT)) \
+{ \
+	LogWrite(LOG_LEVEL_ERROR, __FILE__, __LINE__, FORMAT "%s:", ##__VA_ARGS__, #CONT); \
+}
+#define DebugFor(EXPR, BODY) for EXPR BODY
+#define DebugIf(EXPR, BODY) if (EXPR) BODY
 #endif // NDEBUG
 
 #if !defined(NDEBUG) && defined(LOG_LEVEL_TRACE_ENABLED)
 #define LogTrace(FORMAT, ...) LogWrite(LOG_LEVEL_TRACE, FORMAT, ##__VA_ARGS__)
 #else // !defined(NDEBUG) && defined(LOG_LEVEL_TRACE_ENABLED)
-#define LogTrace(FORMAT, ...)
+#define LogTrace(...)
 #endif // !defined(NDEBUG) && defined(LOG_LEVEL_TRACE_ENABLED)
 
 enum LogLevel
@@ -55,6 +63,11 @@ enum LogLevel
 
 APOKALYPSE3DAPI_EXPORT bool LogInit(const char* filepath, LogLevel logLevel) noexcept;
 APOKALYPSE3DAPI_EXPORT void LogDestroy() noexcept;
+
+#ifdef NDEBUG
 APOKALYPSE3DAPI_EXPORT void LogWrite(LogLevel level, const char* format, ...) noexcept;
+#else // NDEBUG
+APOKALYPSE3DAPI_EXPORT void LogWrite(LogLevel level, const char* file, unsigned line, const char* format, ...) noexcept;
+#endif // NDEBUG
 
 #endif // LOG_H
