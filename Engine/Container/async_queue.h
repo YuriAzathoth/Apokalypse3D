faacfd3fa4285ef10c4ba8d83ad502b7f68dcd63
YuriAzathoth <iamkiller@mail.com>
2022-12-08 16:41:00 +0200

    DEBUG: Added tracing output to async_queue

--- Engine/Container/async_queue.h
+++ Engine/Container/async_queue.h
@@ -35,12 +35,32 @@ extern "C" {
 
 inline static void a3d_async_queue_new(a3d_async_queue_t* queue, unsigned item_size)
 {
+#ifdef A3D_ASYNC_QUEUE_TRACE
+	printf("ASYNC QUEUE NEW\n");
+#endif // A3D_ASYNC_QUEUE_TRACE
 	a3d_circular_queue_new(&queue->queue, item_size);
+#ifdef A3D_ASYNC_QUEUE_TRACE
+	printf("\tPTR  = %llx.\n", queue);
+	printf("\tHEAD = %llx.\n", queue->queue.head);
+	printf("\tTAIL = %llx.\n", queue->queue.tail);
+	printf("\tDATA = %llx.\n", queue->queue.data);
+	printf("\tCAP  = %llx.\n", queue->queue.capacity);
+	printf("\tSIZE = %llx.\n", queue->queue.item_size);
+#endif // A3D_ASYNC_QUEUE_TRACE
 	queue->sync = a3d_mutex_create();
 }
 
 inline static void a3d_async_queue_free(a3d_async_queue_t* queue)
 {
+#ifdef A3D_ASYNC_QUEUE_TRACE
+	printf("ASYNC QUEUE FREE\n");
+	printf("\tPTR  = %llx.\n", queue);
+	printf("\tHEAD = %llx.\n", queue->queue.head);
+	printf("\tTAIL = %llx.\n", queue->queue.tail);
+	printf("\tDATA = %llx.\n", queue->queue.data);
+	printf("\tCAP  = %llx.\n", queue->queue.capacity);
+	printf("\tSIZE = %llx.\n", queue->queue.item_size);
+#endif // A3D_ASYNC_QUEUE_TRACE
 	a3d_circular_queue_free(&queue->queue);
 	a3d_mutex_destroy(queue->sync);
 }
@@ -56,26 +76,65 @@ inline static int a3d_async_queue_empty(a3d_async_queue_t* queue)
 inline static void a3d_async_queue_reserve(a3d_async_queue_t* queue, unsigned count)
 {
 	a3d_mutex_lock(queue->sync);
+#ifdef A3D_ASYNC_QUEUE_TRACE
+	printf("ASYNC QUEUE RESERVE\n");
+#endif // A3D_ASYNC_QUEUE_TRACE
 	a3d_circular_queue_reserve(&queue->queue, count);
+#ifdef A3D_ASYNC_QUEUE_TRACE
+	printf("\tPTR  = %llx.\n", queue);
+	printf("\tHEAD = %llx.\n", queue->queue.head);
+	printf("\tTAIL = %llx.\n", queue->queue.tail);
+	printf("\tDATA = %llx.\n", queue->queue.data);
+	printf("\tCAP  = %llx.\n", queue->queue.capacity);
+	printf("\tSIZE = %llx.\n", queue->queue.item_size);
+	printf("\tReserved %u objects.\n", count);
+#endif // A3D_ASYNC_QUEUE_TRACE
 	a3d_mutex_unlock(queue->sync);
 }
 
 inline static void a3d_async_queue_push(a3d_async_queue_t* queue, const void* src)
 {
 	a3d_mutex_lock(queue->sync);
+#ifdef A3D_ASYNC_QUEUE_TRACE
+	printf("ASYNC QUEUE PUSH\n");
+#endif // A3D_ASYNC_QUEUE_TRACE
 	memcpy(a3d_circular_queue_push(&queue->queue), src, queue->queue.item_size);
+#ifdef A3D_ASYNC_QUEUE_TRACE
+	printf("\tPTR  = %llx.\n", queue);
+	printf("\tHEAD = %llx.\n", queue->queue.head);
+	printf("\tTAIL = %llx.\n", queue->queue.tail);
+	printf("\tDATA = %llx.\n", queue->queue.data);
+	printf("\tCAP  = %llx.\n", queue->queue.capacity);
+	printf("\tSIZE = %llx.\n", queue->queue.item_size);
+#endif // A3D_ASYNC_QUEUE_TRACE
 	a3d_mutex_unlock(queue->sync);
 }
 
 inline static int a3d_async_queue_pop(a3d_async_queue_t* queue, void* dst)
 {
 	a3d_mutex_lock(queue->sync);
+#ifdef A3D_ASYNC_QUEUE_TRACE
+	printf("ASYNC QUEUE POP\n");
+	printf("\tPTR  = %llx.\n", queue);
+	printf("\tHEAD = %llx.\n", queue->queue.head);
+	printf("\tTAIL = %llx.\n", queue->queue.tail);
+	printf("\tDATA = %llx.\n", queue->queue.data);
+	printf("\tCAP  = %llx.\n", queue->queue.capacity);
+	printf("\tSIZE = %llx.\n", queue->queue.item_size);
+#endif // A3D_ASYNC_QUEUE_TRACE
 	const int ret = !a3d_circular_queue_empty(&queue->queue);
 	if (ret)
 	{
+#ifdef A3D_ASYNC_QUEUE_TRACE
+	printf("\tEjecting one item...\n");
+#endif // A3D_ASYNC_QUEUE_TRACE
 		void* ptr = a3d_circular_queue_pop(&queue->queue);
 		memcpy(dst, ptr, queue->queue.item_size);
 	}
+#ifdef A3D_ASYNC_QUEUE_TRACE
+	else
+		printf("\tQueue is empty.\n");
+#endif // A3D_ASYNC_QUEUE_TRACE
 	a3d_mutex_unlock(queue->sync);
 	return ret;
 }
@@ -83,6 +142,9 @@ inline static int a3d_async_queue_pop(a3d_async_queue_t* queue, void* dst)
 inline static void a3d_async_queue_clear(a3d_async_queue_t* queue)
 {
 	a3d_mutex_lock(queue->sync);
+#ifdef A3D_ASYNC_QUEUE_TRACE
+	printf("ASYNC QUEUE CLEAR\n");
+#endif // A3D_ASYNC_QUEUE_TRACE
 	a3d_circular_queue_clear(&queue->queue);
 	a3d_mutex_unlock(queue->sync);
 }
