afba6a4c2fc30fa3d9d45120fc1c6a68c27f4ca3
YuriAzathoth <iamkiller@mail.com>
2023-03-02 16:17:13 +0200

    Bump copyright notice years

--- Engine/Temporary/MeshCacheSystems.cpp
+++ Engine/Temporary/MeshCacheSystems.cpp
@@ -1,6 +1,6 @@
 /*
 	Apokalypse3D - Fast and cache-friendly 3D game engine
-	Copyright (C) 2022 Yuriy Zinchenko
+	Copyright (C) 2022-2023 Yuriy Zinchenko
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
