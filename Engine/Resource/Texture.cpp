e9ac86b675b0dd4d11ce68531c908e2f56520903
YuriAzathoth <iamkiller@mail.com>
2023-03-11 21:02:04 +0200

    Add missing blank line before includes in Texture.cpp

--- Engine/Resource/Texture.cpp
+++ Engine/Resource/Texture.cpp
@@ -15,6 +15,7 @@
 	You should have received a copy of the GNU General Public License
 	along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
+
 #include <bgfx/bgfx.h>
 #include <bimg/decode.h>
 #include <bx/allocator.h>
