2c94ee7cc6f212ef9746980777f744cc02543912
Yuri Zinchenko <iamkiller@mail.com>
2023-03-30 13:15:02 +0300

    Added missing license text to CMake/Apokalypse3D.cmake

--- CMake/Apokalypse3D.cmake
+++ CMake/Apokalypse3D.cmake
@@ -1,3 +1,21 @@
+#
+# Apokalypse3D - Fast and cache-friendly 3D game engine
+# Copyright (C) 2022-2023 Yuriy Zinchenko
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 IF (EMSCRIPTEN)
 	SET_PROPERTY (GLOBAL PROPERTY TARGET_SUPPORTS_SHARED_LIBS TRUE)
 	SET (CMAKE_SHARED_LIBRARY_CREATE_C_FLAGS "-s SIDE_MODULE=1")
